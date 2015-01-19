1082d20a55e4b753551c34d998e83320d19b1bdc
majetyhk <majetyhk@gmail.com>
2015-01-19 22:42:08 +0530

    minor change to logoutLog module

--- handlers/fetch.php
+++ handlers/fetch.php
@@ -138,6 +138,7 @@ error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING ^ E_DEPRECATED);
 		}
 		else
 		{
+			notifyAdmin("Conn.Error:".$conn->error."! In inserting LoginLog for LogId:".$logId,$logId);
 			return false;
 		}
 	}
