2f10a03681b22398f93d0306b0d5521ea591d01b
avinashingit <kavinash366@gmail.com>
2015-02-15 10:46:08 +0530

    mE

--- mailEveryone.php
+++ mailEveryone.php
@@ -1,8 +1,13 @@
 <?php
 
-require_once('QOB/qobConfig.php');
+// require_once('QOB/qobConfig.php');
 
 // require_once('QOB/qob.php');
+// 
+
+mysql_connect("localhost","root","root");
+
+mysql_select_db("iiitdmstudentsportal");
 
 require_once("./PHPMailer_v5.1/class.phpmailer.php");
 
@@ -15,20 +20,13 @@ $results=mysql_query($sql);
 $mailerObj=getMailerObject();
 while($row=mysql_fetch_array($results))
 {
-	echo $row;
-	if($row[0]=="COE12B009")
-	{
-		$email=$row[0]."@iiitdm.ac.in";
-		$subject="Registration starts on Students portal of IIITDM";
-		$content="Please click the following link to register.<br/>";
-		$content.="http://172.17.3.20/4pi/initial.php?userId=".$row[1];
-		// sendEmail($mailerObj,$email,$content,$subject,"");
-	}
+	$email=$row['userId']."@iiitdm.ac.in";
+	$subject="Registration starts on Students portal of IIITDM";
+	$content="Please click the following link to register.<br/>";
+	$content.="http://172.17.3.20/4pi/initial.php?ref=".$row['userIdHash'];
+	sendEmail($mailerObj,$email,$content,$subject,"");
 	
 }
-
-
-
 /*$email="COE11B005"."@iiitdm.ac.in";
 $subject="Registration starts on Students portal of IIITDM";
 $content="Please click the following link to register.<br/>";
